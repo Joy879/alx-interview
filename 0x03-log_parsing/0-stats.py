6f3978409613547669ac11555078ecd7cf335ee6
Joy Wanjiru <70502261+Joy879@users.noreply.github.com>
2022-07-08 04:06:40 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,48 +2,36 @@
 """
 Module 0-stats
 """
-from sys import stdin
+import sys
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-file_size = 0
-status_code = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-    }
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-def print_file_size():
-    """Prints the file size and status codes"""
-    print("File size: {}".format(file_size))
+except Exception as err:
+    pass
 
-    for k, v in sorted(status_code.items()):
-        if v > 0:
-            print("{}: {}".format(k, v))
-
-
-if __name__ == "__main__":
-    """Entry point"""
-
-    try:
-        for k, in_put in enumerate(stdin, 1):
-            try:
-                data = in_put.split()
-                file_size += int(data[-1])
-                if data[-2] in status_code.keys():
-                    status_code[data[-2]] += 1
-
-            except ValueError:
-                pass
-
-            if not k % 10:
-                print_file_size()
-    except KeyboardInterrupt:
-        print_file_size()
-        raise
-    print_file_size()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
